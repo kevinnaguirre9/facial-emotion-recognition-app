7712db21fb20bfac389318202f277ae88d617cc9
kevinnaguirre9 <kevinnaguirre9@gmail.com>
2022-12-31 12:25:24 -0500

    Feature: update emotion recognition model

--- src/Domain/EmotionRecognition/EmotionRecognition.py
+++ src/Domain/EmotionRecognition/EmotionRecognition.py
@@ -12,15 +12,25 @@ class EmotionRecognition(BaseEntity):
             self,
             emotion_recognition_id: EmotionRecognitionId,
             session_id: SessionId,
-            emotion: Emotion,
-            total_students: int,
+            angry: int,
+            disgusted: int,
+            fearful: int,
+            happy: int,
+            neutral: int,
+            sad: int,
+            surprised: int,
             recorded_at: datetime,
     ):
         super().__init__(emotion_recognition_id)
         self.__emotion_recognition_id = emotion_recognition_id
         self.__session_id = session_id
-        self.__emotion = emotion
-        self.__total_students = total_students
+        self.__angry = angry
+        self.__disgusted = disgusted
+        self.__fearful = fearful
+        self.__happy = happy
+        self.__neutral = neutral
+        self.__sad = sad
+        self.__surprised = surprised
         self.__recorded_at = recorded_at
 
 
@@ -28,15 +38,25 @@ class EmotionRecognition(BaseEntity):
     def create(
             emotion_recognition_id: EmotionRecognitionId,
             session_id: SessionId,
-            emotion: Emotion,
-            total_students: int,
+            total_angry: int,
+            total_disgusted: int,
+            total_fearful: int,
+            total_happy: int,
+            total_neutral: int,
+            total_sad: int,
+            total_surprised: int,
             recorded_at: datetime = datetime.now(),
     ):
         return EmotionRecognition(
             emotion_recognition_id,
             session_id,
-            emotion,
-            total_students,
+            total_angry,
+            total_disgusted,
+            total_fearful,
+            total_happy,
+            total_neutral,
+            total_sad,
+            total_surprised,
             recorded_at,
         )
 
@@ -44,29 +64,44 @@ class EmotionRecognition(BaseEntity):
     def emotion_recognition_id(self) -> EmotionRecognitionId:
         return self.__emotion_recognition_id
 
-
     def session_id(self) -> SessionId:
         return self.__session_id
-
-
-    def emotion(self) -> Emotion:
-        return self.__emotion
-
-
-    def total_students(self) -> int:
-        return self.__total_students
-
 
     def recorded_at(self) -> datetime:
         return self.__recorded_at
 
+    def total_angry(self) -> int:
+        return self.__angry
+
+    def total_disgusted(self) -> int:
+        return self.__disgusted
+
+    def total_fearful(self) -> int:
+        return self.__fearful
+
+    def total_happy(self) -> int:
+        return self.__happy
+
+    def total_neutral(self) -> int:
+        return self.__neutral
+
+    def total_sad(self) -> int:
+        return self.__sad
+
+    def total_surprised(self) -> int:
+        return self.__surprised
 
     def to_primitives(self) -> dict:
         return {
             'emotion_recognition_id': self.emotion_recognition_id().value(),
             'session_id': self.session_id().value(),
-            'emotion': self.emotion().value,
-            'total_students': self.total_students(),
+            'angry': self.total_angry(),
+            'disgusted': self.total_disgusted(),
+            'fearful': self.total_fearful(),
+            'happy': self.total_happy(),
+            'neutral': self.total_neutral(),
+            'sad': self.total_sad(),
+            'surprised': self.total_surprised(),
             'recorded_at': self.recorded_at().isoformat(),
         }
 
@@ -76,7 +111,12 @@ class EmotionRecognition(BaseEntity):
         return cls(
             EmotionRecognitionId(emotion_recognition_data['emotion_recognition_id']),
             SessionId(emotion_recognition_data['session_id']),
-            Emotion(emotion_recognition_data['emotion']),
-            emotion_recognition_data['total_students'],
+            emotion_recognition_data['angry'],
+            emotion_recognition_data['disgusted'],
+            emotion_recognition_data['fearful'],
+            emotion_recognition_data['happy'],
+            emotion_recognition_data['neutral'],
+            emotion_recognition_data['sad'],
+            emotion_recognition_data['surprised'],
             datetime.fromisoformat(emotion_recognition_data['recorded_at']),
         )
--- src/Services/EmotionRecognition/Register/EmotionRecognitionRegister.py
+++ src/Services/EmotionRecognition/Register/EmotionRecognitionRegister.py
@@ -1,56 +1,30 @@
-from datetime import datetime
-
 from src.Services.EmotionRecognition.Register.RegisterEmotionRecognitionCommand import RegisterEmotionRecognitionCommand
 from src.Domain.EmotionRecognition.Contracts.EmotionRecognitionRepository import EmotionRecognitionRepository
 from src.Domain.EmotionRecognition.EmotionRecognition import EmotionRecognition
-from src.Services.EmotionRecognition.Recognize.EmotionRecognizer import EmotionRecognizer
-from src.Services.EmotionRecognition.Recognize.FacesRecognizer import FacesRecognizer
 from src.Domain.EmotionRecognition.ValueObjects.EmotionRecognitionId import EmotionRecognitionId
 from src.Domain.Session.ValueObjects.SessionId import SessionId
 
 
 class EmotionRecognitionRegister:
 
-    def __init__(
-            self,
-            emotion_recognition_repository: EmotionRecognitionRepository,
-            faces_recognizer: FacesRecognizer,
-            emotion_recognizer: EmotionRecognizer,
-    ):
+    def __init__(self, emotion_recognition_repository: EmotionRecognitionRepository):
         self.__emotion_recognition_repository = emotion_recognition_repository
-        self.__faces_recognizer = faces_recognizer
-        self.__emotion_recognizer = emotion_recognizer
 
 
     def handle(self, command: RegisterEmotionRecognitionCommand):
 
-        frame = command.frame()
-
         session_id = SessionId(command.session_id())
 
-        recorded_time = datetime.now()
-
-        faces = self.__faces_recognizer.recognize(frame)
-
-        emotions_recognized = list(map(self.__emotion_recognizer.recognize, faces))
-
-        #Get total students matching each recognized emotion
-        emotions_recognized_count = dict(
-            (emotion, emotions_recognized.count(emotion))
-            for emotion in set(emotions_recognized)
+        emotion_recognition = EmotionRecognition.create(
+            EmotionRecognitionId(),
+            session_id,
+            command.total_angry(),
+            command.total_disgusted(),
+            command.total_fearful(),
+            command.total_happy(),
+            command.total_neutral(),
+            command.total_sad(),
+            command.total_surprised(),
         )
 
-        #Save the records!
-        for emotion, total_students in emotions_recognized_count.items():
-
-            emotion_recognition = EmotionRecognition.create(
-                EmotionRecognitionId(),
-                session_id,
-                emotion,
-                total_students,
-                recorded_time
-            )
-
-            self.__emotion_recognition_repository.save(emotion_recognition)
-
-        #todo: return frame with emotions recognized in each face
+        self.__emotion_recognition_repository.save(emotion_recognition)
